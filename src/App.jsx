9a7ce94749e58c55fe12742a2a7c7dfb682d9f98
Jegan <jeganbaskar19@gmail.com>
2026-08-12 22:29:32 +0530

    feat(app): protect routes with HumanVerificationGate component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Resume from './pages/Resume/Resume';
+import HumanVerificationGate from './components/BotShield/HumanVerificationGate';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/resume" element={<Resume />} />
-    </Routes>
+    <HumanVerificationGate>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/resume" element={<Resume />} />
+      </Routes>
+    </HumanVerificationGate>
   );
 }
 
